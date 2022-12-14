1154fa9a38351fcd027b9ccdae69cb0c885606c1
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2022-12-13 19:29:10 -0600

    initiate

---
+++ main.js
@@ -0,0 +1,85 @@
+// Create a stopwatch object that has 4 properties
+//  and 4 method
+
+let stopWatch = {}
+
+stopWatch.shape = 'square'
+stopWatch.color = 'black'
+stopWatch.size = 'medium'
+stopWatch.brand = 'Rolex'
+
+stopWatch.start = function () {
+    console.log('Startinggggg!!!')
+}
+stopWatch.stop = function () {
+    console.log('Stoppingggg!!!')
+}
+stopWatch.beep = function () {
+    console.log('Beepingggg!!')
+}
+stopWatch.reset = function () {
+    console.log('Now Reseting!')
+}
+
+// let stopWatch = {}
+
+// stopWatch.shape = 'square'
+// stopWatch.color = 'red'
+// stopWatch.size = 'small'
+// stopWatch.brand = 'Timex'
+
+// stopWatch.start = function(){
+//     console.log('STARTINGGG...')
+// }
+
+// stopWatch.stop = function(){
+//     console.log('STOPPPINGGG')
+// }
+
+// stopWatch.beep = function(){
+//     console.log('Beep!...Beep!...Beep!...')
+// }
+
+
+
+
+// stopWatch.currentTime = 12 
+
+// stopWatch.tellTime = function(time){
+//     console.log(`The current time is ${time}.`)
+// }
+// stopWatch.tellTime(stopWatch.currentTime)
+
+
+
+
+
+
+// let stopWatch = {}
+
+// stopWatch.currentTime = 12
+
+// stopWatch.tellTime = function(time) {
+//     console.log(`The current time is ${time}.`)
+// }
+
+// stopWatch.tellTime(stopWatch.currentTime)
+
+// let stopwatch = {}
+
+// stopwatch.brand = 'Nike'
+// stopwatch.color = 'black'
+// stopwatch.shape = 'round'
+// stopwatch.size = 'medium'
+
+// stopwatch.start = function () {
+//     console.log('STARTING')
+// }
+
+// stopwatch.stop = function() {
+//     console.log('STOPPING')
+// }
+
+// stopwatch.sayBrand = function () {
+//     console.log( stopwatch.brand )
+// }
